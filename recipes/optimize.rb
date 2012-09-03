a7a43766af8c92a4bd66df6684e7bf3fdecae857
Sebastian Johnsson <sebastian@agiley.se>
2012-09-03 16:54:32 +0200

    Just copy data/log files to same folder and suffix with .bak

--- recipes/optimize.rb
+++ recipes/optimize.rb
@@ -14,14 +14,13 @@ if (node['mysql']['perform_optimization'])
   end
   
   bash "backup_current_data_and_log_files" do
-    backup_folder   =   "backup"
+    data_file_path  =   "#{node['mysql']['data_dir']}/ibdata1"
     
-    code "sudo mkdir -p #{node['mysql']['data_dir']}/#{backup_folder}"
-    code "if test -e #{node['mysql']['data_dir']}/ibdata1; then sudo mv #{node['mysql']['data_dir']}/ibdata1 #{node['mysql']['data_dir']}/#{backup_folder}/ibdata1; fi;"
+    code "if test -e #{data_file_path}; then sudo mv #{data_file_path} #{data_file_path}.bak; fi;"
 
     0.upto(node['mysql']['tunable']['innodb_log_files_in_group'] - 1) do |i|
       file_path     =   "#{node['mysql']['data_dir']}/ib_logfile#{i}"
-      code "if test -e #{file_path}; then sudo mv #{file_path} #{node['mysql']['data_dir']}/#{backup_folder}/ib_logfile#{i}; fi;"
+      code "if test -e #{file_path}; then sudo mv #{file_path} #{file_path}.bak; fi;"
     end
   end
   
