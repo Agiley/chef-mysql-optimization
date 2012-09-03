95f431c3431c30efeb1aa066df937d941dce77c6
Sebastian Johnsson <sebastian@agiley.se>
2012-09-03 17:32:41 +0200

    Fix typo, should be 'stop' and not 'restart'

--- recipes/optimize.rb
+++ recipes/optimize.rb
@@ -9,7 +9,7 @@ if (node['mysql']['perform_optimization'])
   end
   
   #We need to stop MySQL and then backup its datafile and logfiles before changing the innodb_log_file_size-setting. Otherwise MySQL won't start.
-  bash 'restart_mysql' do
+  bash 'stop_mysql' do
     code "echo 'Stopping MySQL in order to backup data and log files.'"
     notifies :stop, resources(:service => "mysql"), :immediately
   end
