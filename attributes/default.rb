50d7c2dd86b22d6e791599d7aae7c83bf6b85ede
Sebastian Johnsson <sebastian@agiley.se>
2012-09-04 00:02:07 +0200

    Override previous defined tunable attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,42 +10,39 @@ default['mysql']['admin_user']                                  =   {
   'password'    =>  nil
 }
 
-default['mysql']['tunable']['default-storage-engine']           =   'innodb'
+override['mysql']['tunable']['default-storage-engine']           =   'innodb'
 
-default['mysql']['tunable']['connect_timeout']                  =   10
-default['mysql']['tunable']['interactive_timeout']              =   180
+override['mysql']['tunable']['connect_timeout']                  =   10
+override['mysql']['tunable']['interactive_timeout']              =   180
 #wait_timeout is defined in the original MySQL cookbook
 
-default['mysql']['tunable']['enable_binary_logging']            =   false
-default['mysql']['tunable']['server-id']                        =   1
-default['mysql']['tunable']['log_bin']                          =   '/var/log/mysql/mysql-bin.log'
+override['mysql']['tunable']['enable_binary_logging']            =   false
+override['mysql']['tunable']['server-id']                        =   1
+override['mysql']['tunable']['log_bin']                          =   '/var/log/mysql/mysql-bin.log'
 
-default['mysql']['tunable']['innodb_flush_log_at_trx_commit']   =   0
-default['mysql']['tunable']['innodb_thread_concurrency']        =   10
-default['mysql']['tunable']['innodb_flush_method']              =   'O_DIRECT'
-default['mysql']['tunable']['innodb_file_per_table']            =   true
-default['mysql']['tunable']['innodb_log_files_in_group']        =   2
-default['mysql']['tunable']['innodb_log_buffer_size']           =   '4M'
-default['mysql']['tunable']['innodb_log_file_size']             =   '128M'
+override['mysql']['tunable']['innodb_flush_log_at_trx_commit']   =   0
+override['mysql']['tunable']['innodb_thread_concurrency']        =   10
+override['mysql']['tunable']['innodb_flush_method']              =   'O_DIRECT'
+override['mysql']['tunable']['innodb_file_per_table']            =   true
+override['mysql']['tunable']['innodb_log_files_in_group']        =   2
+override['mysql']['tunable']['innodb_log_buffer_size']           =   '4M'
+override['mysql']['tunable']['innodb_log_file_size']             =   '128M'
+override['mysql']['tunable']['innodb_additional_mem_pool_size']  =   "128M"
 
 #Because of file_per_table = true. See http://mysqldump.azundris.com/archives/78-Configuring-InnoDB-An-InnoDB-tutorial.html
-default['mysql']['tunable']['innodb_data_file_path']            =   'ibdata1:10M:autoextend' 
-default['mysql']['tunable']['innodb_autoextend_increment']      =   8
+override['mysql']['tunable']['innodb_data_file_path']            =   'ibdata1:10M:autoextend' 
+override['mysql']['tunable']['innodb_autoextend_increment']      =   8
 
 #For a 1024mb memory VPS
-default['mysql']['tunable']['query_cache_size']                 =   "256M"
-default['mysql']['tunable']['query_cache_limit']                =   "128M"
-default['mysql']['tunable']['tmp_table_size']                   =   "128M"
-default['mysql']['tunable']['key_buffer_size']                  =   "32M"
-default['mysql']['tunable']['read_buffer_size']                 =   "62M"
-default['mysql']['tunable']['read_rnd_buffer_size']             =   "62M"
-default['mysql']['tunable']['sort_buffer_size']                 =   "32M"
-default['mysql']['tunable']['join_buffer_size']                 =   "32M"
-default['mysql']['tunable']['bulk_insert_buffer_size']          =   "32M"
+override['mysql']['tunable']['query_cache_size']                 =   "256M"
+override['mysql']['tunable']['query_cache_limit']                =   "128M"
+override['mysql']['tunable']['tmp_table_size']                   =   "128M"
+override['mysql']['tunable']['key_buffer_size']                  =   "32M"
+override['mysql']['tunable']['read_buffer_size']                 =   "62M"
+override['mysql']['tunable']['read_rnd_buffer_size']             =   "62M"
+override['mysql']['tunable']['sort_buffer_size']                 =   "32M"
+override['mysql']['tunable']['join_buffer_size']                 =   "32M"
+override['mysql']['tunable']['bulk_insert_buffer_size']          =   "32M"
                                                                                     
-default['mysql']['tunable']['myisam_sort_buffer_size']          =   "62M"
-default['mysql']['tunable']['myisam_max_sort_file_size']        =   "62M"
-                                                                                    
-default['mysql']['tunable']['innodb_additional_mem_pool_size']  =   "128M"
-default['mysql']['tunable']['innodb_log_buffer_size']           =   "4M"
-default['mysql']['tunable']['innodb_log_file_size']             =   "128M"
+override['mysql']['tunable']['myisam_sort_buffer_size']          =   "62M"
+override['mysql']['tunable']['myisam_max_sort_file_size']        =   "62M"
