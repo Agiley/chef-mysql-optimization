c812bd7dd12d3ae8f2cbefd794eb72829fec225c
Sebastian Johnsson <sebastian@agiley.se>
2014-06-26 12:49:34 +0200

    Upd

--- recipes/optimize.rb
+++ recipes/optimize.rb
@@ -12,10 +12,12 @@ if (node['mysql']['perform_optimization'])
   end
 
   #We need to stop MySQL and then backup its datafile and logfiles before changing the innodb_log_file_size-setting. Otherwise MySQL won't start again.
-  bash 'force_mysql_stop' do
-    code "echo 'Forced stop of MySQL in order to backup data and log files.'"
-    notifies :stop, 'mysql_service', :immediately
-  end
+  #bash 'force_mysql_stop' do
+  #  code "echo 'Forced stop of MySQL in order to backup data and log files.'"
+  #  notifies :stop, 'mysql_service', :immediately
+  #end
+  
+  execute "service mysql stop"
   
   bash "remove_current_data_file_and_log_files" do
     code <<-EOH
