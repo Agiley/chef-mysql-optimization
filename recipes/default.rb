72f3dc1b11b200ac930a9ae48e8589f8a34a7d1c
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2016-05-15 19:23:10 +0700

    Deprecate secure_installation (already taken care of in primary MySql cookbook)

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
 include_recipe "mysql::server"
-include_recipe "mysql-optimization::secure_mysql"
+#include_recipe "mysql-optimization::secure_mysql" Deprecated: Already taken care of in primary mysql cookbook
 include_recipe "mysql-optimization::create_admin_user"
 include_recipe "mysql-optimization::optimize"
