17e404122032a52093d2ab33e58f29cbbc1a29de
Sebastian <sebastian.johnsson@gmail.com>
2018-03-14 19:39:12 +0100

    Remove dependency on old deprecated recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,3 @@
-include_recipe "mysql::server"
 #include_recipe "mysql-optimization::secure_mysql" Deprecated: Already taken care of in primary mysql cookbook
 include_recipe "mysql-optimization::create_admin_user"
 include_recipe "mysql-optimization::optimize"
